feca61b1612f16851ab9e84b96aa14fd299110b2
satyamjha <satyamjhaactor07@gmail.com>
2025-03-09 19:53:47 +0530

    Done Projects

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,95 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name1: "",
+      name2: "",
+      result: "",
+    };
+  }
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+  calculateFLAMES = () => {
+    const { name1, name2 } = this.state;
+
+    if (!name1.trim() || !name2.trim()) {
+      this.setState({ result: "Please Enter valid input" });
+      return;
     }
-}
 
+    let str1 = name1.split("");
+    let str2 = name2.split("");
+
+    // Remove common letters
+    str1.forEach((char) => {
+      const index = str2.indexOf(char);
+      if (index !== -1) {
+        str2.splice(index, 1);
+      }
+    });
+
+    const remainingLength = str1.length + str2.length;
+    const flamesResult = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
+
+    this.setState({ result: flamesResult[remainingLength % 6] });
+  };
+
+  clearFields = () => {
+    this.setState({ name1: "", name2: "", result: "" });
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <h1>FLAMES Relationship Calculator</h1>
+
+        <input
+          type="text"
+          data-testid="input1"
+          name="name1"
+          value={this.state.name1}
+          onChange={this.handleInputChange}
+          placeholder="Enter first name"
+        />
+
+        <input
+          type="text"
+          data-testid="input2"
+          name="name2"
+          value={this.state.name2}
+          onChange={this.handleInputChange}
+          placeholder="Enter second name"
+        />
+
+        <button
+          data-testid="calculate_relationship"
+          name="calculate_relationship"
+          onClick={this.calculateFLAMES}
+        >
+          Calculate Relationship
+        </button>
+
+        <button
+          data-testid="clear"
+          name="clear"
+          onClick={this.clearFields}
+        >
+          Clear
+        </button>
+
+        {this.state.result && (
+          <h3 data-testid="answer">{this.state.result}</h3>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
